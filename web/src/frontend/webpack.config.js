436ae9f9fb11c16ec69265fed95479cb812b0c71
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2023-10-07 02:28:47 +0900

    build settings

--- web/src/frontend/webpack.config.js
+++ web/src/frontend/webpack.config.js
@@ -1,17 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path')
+const fs = require('fs')
+const glob = require('glob')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+// Get entry points as directory-names under src/pages
+function getEntry() {
+  const pages = fs.readdirSync(path.join(__dirname, 'src/pages')).filter((file) => {
+    return fs.statSync(path.join(__dirname, 'src/pages', file)).isDirectory()
+  })
+  const entry = {}
+  for (const page of pages) {
+    const file = path.resolve(__dirname, `src/pages/${page}/index.ts`)
+    if (fs.existsSync(file)) {
+      entry[page] = file
+    }
+  }
+  return entry
+}
+
+// Get html files under /src in order to move them to ../main/resources/templates
+function getHtmlWebpackPlugin() {
+  const htmlFiles = glob.sync(path.resolve(__dirname, 'src/**/*.html'))
+  const entry = []
+  for (const file of htmlFiles) {
+    entry.push(
+      new HtmlWebpackPlugin({
+        inject: false,
+        filename: `templates/${file.replace(/.*frontend\/src\//, '')}`,
+        template: file,
+      })
+    )
+  }
+  return entry
+}
+
+
 
 module.exports = {
-  entry: {
-    'demo': path.resolve(__dirname, './src/pages/demo/index.ts')
-  },
+  entry: getEntry(),
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'static/pages/[name]/index.js',
   },
   resolve: {
-    extensions: ['.ts'],
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
@@ -27,11 +60,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-			filename: 'templates/pages/demo/index.html',
-			template: './src/pages/demo/index.html',
-		}),
+    ...getHtmlWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'static/pages/[name]/index.css',
     }),
