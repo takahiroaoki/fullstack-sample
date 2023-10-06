64d252fd8bac2204459bf58251da9bc18be04978
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2023-10-06 22:42:20 +0900

    initial commit

---
+++ web/src/test/java/jp/co/company/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.company.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
